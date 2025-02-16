97726aee124a92b607aaf0aaf17e269453895d56
rayen103 <rayen.farhani@esprit.tn>
2025-02-16 22:19:56 +0100

    add notification

---
+++ microservices/notification/src/main/java/com/example/notification/controller/NotificationController.java
@@ -0,0 +1,34 @@
+package com.example.notification.controller;
+
+
+import com.example.notification.entities.Notification;
+import com.example.notification.entities.NotificationStatus;
+import com.example.notification.services.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notifications")
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @PostMapping
+    public Notification createNotification(@RequestBody Notification notification) {
+        return notificationService.sendNotification(notification);
+    }
+
+    @GetMapping("/user/{userId}")
+    public List<Notification> getNotifications(@PathVariable Long userId) {
+        return notificationService.getNotificationsByUserId(userId);
+    }
+
+    @PutMapping("/{id}/status")
+    public void updateStatus(@PathVariable Long id, @RequestParam NotificationStatus status) {
+        notificationService.updateNotificationStatus(id, status);
+    }
+}
+
---
+++ microservices/notification/src/main/java/com/example/notification/entities/Notification.java
@@ -0,0 +1,26 @@
+package com.example.notification.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+    private String type;
+    private String message;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationStatus status;
+}
